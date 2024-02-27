63e3eee3e1c306fe926c640ae1deaea9f1c86b77
Davide Bonifacio <web@davidebonifacio.it>
2024-02-27 14:30:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  // creo la struttura html / CSS e i componenti come da esempio nel file .jpg
+  // carico i tasks dal file data/tasks.js
+  // mostro i tasks nella webapp
+  // integro la logica del pulsante delete
+  // Suggerimento: posso mettere l'array dei tasks dentro a useState
+  // let [] = useState(tasks)
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ src/data/tasks.js
@@ -0,0 +1,6 @@
+let tasks = [
+  {id:1, name: 'Studia React', isCompleted: false},
+  {id:2, name: 'Studia CSS', isCompleted: true},
+  {id:3, name: 'Fai la spesa', isCompleted: true},
+  {id:4, name: 'Porta fuori il cane', isCompleted: false},
+]
