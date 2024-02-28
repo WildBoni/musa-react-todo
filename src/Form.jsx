f0b4e2e24ec2e9cb86f41408eb56c17dfa2d08ae
Davide Bonifacio <web@davidebonifacio.it>
2024-02-28 14:21:37 +0100

    form with enter

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,16 +3,20 @@ import { useRef } from "react"
 export default function Form({addTask}) {
   const inputRef = useRef();
 
-  function handleAddTask() {
-    addTask(inputRef.current.value)
-    inputRef.current.value = '';
+  function handleAddTask(event) {
+    if(event.key === 'Enter' || event.type === 'click') {
+      if(inputRef.current.value) {
+        addTask(inputRef.current.value)
+        inputRef.current.value = '';
+      }
+    }
   }
   
   return (
     <>
       <h4>Che cosa devi fare?</h4>
-      <input type="text" ref={inputRef} />
-      <button onClick={() => handleAddTask()}>Aggiungi</button>
+      <input type="text" ref={inputRef} onKeyUp={(event) => handleAddTask(event)}/>
+      <button onClick={(event) => handleAddTask(event)}>Aggiungi</button>
     </>
   )
 }
