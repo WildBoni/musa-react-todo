528663504367db1bcb51048f19e8d5357c20b801
Davide Bonifacio <web@davidebonifacio.it>
2024-02-29 10:57:00 +0100

    changed title

--- src/Task.jsx
+++ src/Task.jsx
@@ -2,7 +2,7 @@ export default function Task({taskDetails, toggleCompleted, deleteTask}) {
   // ricevo i dettagli del task (id, name, isCompleted)
   // ricevo anche la funzione da chiamare quando si cancella un task, a cui passo l'id del task che voglio eliminare
   return (
-    <li className={taskDetails.isCompleted ? "task-completed" : 'task'}>
+    <li className={taskDetails.isCompleted ? "task-completed" : "task"}>
       <div>
         <input 
           type="checkbox" defaultChecked={taskDetails.isCompleted}
